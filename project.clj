dc23d6f0e1c3ac689ecfcc0b5be00ef66f767d7f
Timothy Washington <twashing@gmail.com>
2014-04-13 07:50:13 -0700

    lein-release plugin: preparing 0.1.5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject garden-watch "0.1.5-SNAPSHOT"
+(defproject garden-watch "0.1.5"
   :description "Garden source to CSS watcher and converter"
   :url "https://github.com/twashing/garden-watch"
   :license {:name "Eclipse Public License"
