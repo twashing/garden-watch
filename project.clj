f8c834eba062f689dae78905619c1e51e72304cb
Timothy Washington <twashing@gmail.com>
2014-04-13 07:50:23 -0700

    lein-release plugin: bumped version from 0.1.5 to 0.1.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject garden-watch "0.1.5"
+(defproject garden-watch "0.1.6-SNAPSHOT"
   :description "Garden source to CSS watcher and converter"
   :url "https://github.com/twashing/garden-watch"
   :license {:name "Eclipse Public License"
