ac560d334fc35880baa08cef42306765a5a1a527
Timothy Washington <twashing@gmail.com>
2014-04-13 07:34:45 -0700

    lein-release plugin: preparing 0.1.4 release

--- project.clj
+++ project.clj
@@ -1,17 +1,18 @@
-(defproject garden-watch "0.1.4-SNAPSHOT"
+(defproject garden-watch "0.1.4"
   :description "Garden source to CSS watcher and converter"
   :url "https://github.com/twashing/garden-watch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/timbre "3.1.6"]
+  :eval-in-leiningen true
+  
+  :dependencies [[com.taoensso/timbre "3.1.6"]
                  [filevents "0.1.0"]
-                 [zengarden "0.1.5"]]
+                 [zengarden "0.1.6"]]
 
-  :profiles {:dev {:source-paths ["dev"]
+  :profiles {:dev {:source-paths ["dev" "src"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [missing-utils "0.1.1"]
                                   [alembic "0.2.1"]]}}
 
   :lein-release {:deploy-via :clojars})
+
